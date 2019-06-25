ab931988d830b5f7a015e5d4d4c7bdc6428d6303
Felipe Gonzalez <felipexgonzalez@gmail.com>
2019-06-24 20:32:43 -0500

    Improve lights when sleeping and watching tv

---
+++ appdaemon/config/apps/motion_lights_s.py
@@ -0,0 +1,99 @@
+import appdaemon.plugins.hass.hassapi as hass
+import json
+
+
+
+class MotionLights(hass.Hass):
+
+
+  def initialize(self):
+    # this dictionary keeps timers for groups
+    self.timer_handle = {}
+    self.sleep_mode = self.get_state("input_boolean.sleep_mode")
+    # data
+    self.original_grouping = json.loads(self.args["grouping"])
+    self.grouping = json.loads(self.args["grouping"])
+    print(self.grouping)
+    # Suscribe to sensors
+    motion_sensors = self.grouping.keys()
+    for sensor in motion_sensors:
+      self.timer_handle[self.grouping[sensor]["lights"]] = None
+      # set brightness states
+      if('brightness' in self.grouping[sensor].keys()):
+        pass
+      else:
+        self.grouping[sensor]["brightness"] = 255
+        self.original_grouping[sensor]["brightness"] = 255
+
+      if("media" in self.grouping[sensor].keys()):
+        self.listen_state(self.adapt_brightness, self.grouping[sensor]["media"])
+
+      # listen to motion sensors
+      self.listen_state(self.motion, sensor, 
+                        group = self.grouping[sensor]["lights"], 
+                        mode = self.grouping[sensor]["use_sleep"],
+                        delay = self.grouping[sensor]["delay"])
+    # listen to sleep mode and tv watching
+    self.listen_state(self.adapt_brightness, "input_boolean.sleep_mode")
+
+  def adapt_brightness(self, entity, attribute, old, new, kwargs):
+    self.sleep_mode = self.get_state("input_boolean.sleep_mode")
+    # when going into sleep mode:
+    if(entity == "input_boolean.sleep_mode" and new == "on"):
+      for sensor in self.grouping.keys():
+        if(self.grouping[sensor]["use_sleep"] == "dim"):
+          self.grouping[sensor]["brightness"] = 40
+        if(self.grouping[sensor]["use_sleep"] == "dark"):
+          self.turn_off(group = self.grouping[sensor]["lights"], transition = 20)
+          self.grouping[sensor]["brightness"] = 0
+    if(entity == "input_boolean.sleep_mode" and new == "off"):
+      # restore brightness values
+      for sensor in self.grouping.keys():          
+        self.grouping[sensor]["brightness"] = self.original_grouping[sensor]["brightness"]
+    
+    #### # adapt brightness for tv watching
+    if(entity == "media_player.bedroom_apple_tv" and sleep_mode == "off"):
+      if(old == "playing"):
+        self.grouping["binary_sensor.motion_recamara"]["brightness"] = 255
+      if(new == "playing"):
+        self.grouping["binary_sensor.motion_recamara"]["brightness"] = 25
+      group = self.grouping["binary_sensor.motion_recamara"]["lights"]
+      self.log("Adapting lights for TV watching in bedroom.")
+      self.turn_on(group, brightness = self.grouping["binary_sensor.motion_recamara"]["brightness"], 
+          transition = 0, kelvin = 2500)
+
+  def motion(self, entity, attribute, old, new, kwargs):
+    group = kwargs.get('group', None)
+    use_sleep = kwargs.get('mode', None)
+    delay = kwargs.get('delay', None)
+    if new == "on" and old == "off":
+      self.log("Motion detected: turning {} on".format(group))
+      if(self.sleep_mode == "off" or use_sleep != "dark"):
+        self.turn_on(group, brightness = self.grouping[entity]["brightness"], 
+          transition = 0, kelvin = 2500)
+      self.cancel_timer(self.timer_handle[group])
+    if new == "off" and old == "on":
+      # set timer to turn off
+      self.timer_handle[group] = self.run_in(self.light_off, delay, group = group, sensor = entity)
+  
+  def light_off(self, kwargs):
+    #if "entity_off" in self.args:
+    group = kwargs.get('group', None)
+    sensor = kwargs.get('sensor', None)
+    transition = kwargs.get('transition', 30)
+    #self.log()
+    if(self.get_state(sensor) == 'off'):
+      self.log("Turning {} off".format(group))
+      self.turn_off(group, transition = transition)
+      self.cancel_timer(self.timer_handle[group])
+      self.timer_handle[group] = self.run_in(self.light_off, 60*5, transition = 0, group = group)
+    #else:
+    #  bright = 255
+    #  self.turn_on(group, brightness = bright)
+    #  self.cancel_timer(self.handle_group[group])
+    #  self.handle_group[group] = self.run_in(self.light_off, 120, group = group, entity = entity)
+
+  def cancel(self, kwargs):
+    group = kwargs.get('group', None)
+    self.cancel_timer(self.timer_handle[group])
+      
